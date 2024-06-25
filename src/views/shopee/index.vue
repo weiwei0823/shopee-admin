4a4f2de0b18d6d0cfb46d10389948763df39fb14
刘德伟 <liudewei0616@gmail.com>
2024-06-25 22:21:42 +0800

    虾皮数据切换修改

--- src/views/shopee/index.vue
+++ src/views/shopee/index.vue
@@ -1,7 +1,7 @@
 <template>
-  <yu-layout title="虾皮">
+  <yu-layout title="虾皮" class="shopee-container">
     <template #body>
-      <div class="section-container">
+      <div class="shopee">
         <!--筛选区域-->
         <el-divider content-position="left">筛选区域</el-divider>
         <div class="flex flex-wrap gap-4 items-center">
@@ -24,29 +24,56 @@
           <!--店铺-->
         </div>
         <el-divider content-position="left">店铺列表</el-divider>
-        <el-table v-loading="listLoading" :data="shopObj.list" border fit highlight-current-row style="width: 100%">
-          <el-table-column align="center" label="名称" width="100">
+        <el-table v-loading="listLoading"
+                  :key="`${categoryObj.current}_${pageObj.index}`"
+                  :data="shopObj.list"
+                  border
+                  fit
+                  highlight-current-row
+                  class="shopTable"
+                  style="width: 100%">
+          <!--名称-->
+          <el-table-column align="center" label="名称" min-width="200">
             <template #default="{ row }">
-              <span>{{ row.name }}</span>
+              <el-tooltip placement="top">
+                <span class="shopTable-column">{{ row.name }}</span>
+                <template #content>
+                  {{ row.name }}
+                </template>
+              </el-tooltip>
             </template>
           </el-table-column>
-
-          <el-table-column width="180px" align="center" label="描述">
+          <!--价格-->
+          <el-table-column align="center" label="价格" min-width="200">
             <template #default="{ row }">
-              <span>{{ row.description }}</span>
+              <el-tooltip placement="top">
+                <span class="shopTable-column">{{ row.price }}</span>
+                <template #content>
+                  <div class="shopTable-column">最低价：{{ row.price_min }}</div>
+                  <div class="shopTable-column">最高价: {{ row.price_max }}</div>
+                </template>
+              </el-tooltip>
             </template>
           </el-table-column>
-
-          <el-table-column width="110px" align="center" label="店铺地址">
+          <!--描述-->
+          <el-table-column align="center" label="描述" min-width="200">
             <template #default="{ row }">
-              <span>{{ row.store_place }}</span>
+              <span class="shopTable-column">{{ row.description }}</span>
             </template>
           </el-table-column>
-
-          <el-table-column width="100px" label="图片">
+          <!--店铺地址-->
+          <el-table-column align="center" label="店铺地址" min-width="200">
             <template #default="{ row }">
-              <span>
-                <img :src="row.main_image[0]" alt="" />
+              <span class="shopTable-column">{{ row.store_place }}</span>
+            </template>
+          </el-table-column>
+          <!--图片-->
+          <el-table-column label="图片" min-width="200">
+            <template #default="{ row }">
+              <span class="shopTable-column">
+                <img class="shopTable-column-img"
+                     :src="row.main_image ? row.main_image[0] : ''"
+                     alt=""/>
               </span>
             </template>
           </el-table-column>
@@ -87,13 +114,13 @@ const countryObj = reactive( {
 // 商品分类
 const categoryObj = reactive( {
   list : [],
-  fileList : [],
-  current : '',
-  currentFile : ''
+  current : ''
 } )
 // 店铺列表
 const shopObj = reactive( {
+  all : {}, // 当前国家下的所有category对应的shop列表
   list : [],
+  pageList : [],
   current : ''
 } )
 
@@ -104,6 +131,8 @@ const pageObj = reactive( {
 } )
 // 获取商品类别列表
 const getCategoryList = () => {
+  categoryObj.list = []
+  categoryObj.current = ''
   listLoading.value = true
   let modulesFiles = []
   switch ( countryObj.current ) {
@@ -123,15 +152,15 @@ const getCategoryList = () => {
       return
   }
   // 获取商品分类信息
-  Object.keys( modulesFiles ).forEach( item => {
-    categoryObj.fileList.push( modulesFiles[item]?.default || [] )
-    const tempStr = RegExp( `(?<=products_${countryObj.current}\/).+?(?=\\.json)` ).exec( item )[0]
+  Object.keys( modulesFiles ).forEach( keyItem => {
+    const tempStr = RegExp( `(?<=products_${countryObj.current}\/).+?(?=\\.json)` ).exec( keyItem )[0]
     categoryObj.list.push( {
       value : tempStr,
-      label : tempStr
+      label : tempStr,
+      originalKey : keyItem
     } )
+    shopObj.all[tempStr] = modulesFiles[keyItem]?.default || []
   } )
-  categoryObj.currentFile = categoryObj.fileList[0]
   categoryObj.current = categoryObj.list[0]?.value
   getShopList()
   listLoading.value = false
@@ -139,21 +168,46 @@ const getCategoryList = () => {
 
 // 获取商品类别列表
 const getShopList = () => {
+  shopObj.list = []
+  shopObj.pageList = []
+  shopObj.current = ''
   listLoading.value = true
-  shopObj.list = categoryObj.fileList?.filter( ( item, index ) => {
+  shopObj.list = shopObj.all[categoryObj.current]
+  shopObj.pageList = shopObj.list?.filter( ( item, index ) => {
     return +index >= ( pageObj?.index - 1 ) * pageObj?.size && +index < pageObj?.index * pageObj?.size
   } )
   listLoading.value = false
 }
 
-const changeCountry = value => {
+const changeCountry = () => {
+  shopObj.all = {}
   getCategoryList()
 }
-const changeCategory = value => {}
+const changeCategory = () => {
+  pageObj.index = 1
+  getShopList()
+}
 getCategoryList()
 defineOptions( {
   name : 'shopee'
 } )
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.shopee {
+  .shopTable {
+    width: 100%;
+
+    &-column {
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+
+      &-img {
+        height: 100px;
+        object-fit: contain;
+      }
+    }
+  }
+}
+</style>
