a1bc601fcb9ec4984d67dca7f3c3d699443eff8d
刘德伟 <liudewei0616@gmail.com>
2024-09-04 23:12:01 +0800

    提交信息

--- src/api/bigSeller.js
+++ src/api/bigSeller.js
@@ -1,22 +1,22 @@
 import httpBigSeller from '@/utils/bigsellerRequest'
 
-const request = createAxiosByInterceptors( {
-  baseURL : 'https://www.bigseller.com/api/v1'
-} )
+const request = createAxiosByInterceptors({
+  baseURL: 'https://www.bigseller.com/api/v1',
+})
 
-export function getDraftList( params ) {
-  return httpBigSeller.request( {
-    method : 'get',
-    url : '/product/global/shopee/draft.json',
-    params : {
-      bsStatus : 1,
-      shopId : '',
-      pageNo : 1,
-      pageSize : 50,
-      inquireType : 0,
-      searchType : '',
-      searchContent : '',
-      ...params
-    }
-  } )
+export function getDraftList(params) {
+  return httpBigSeller.request({
+    method: 'get',
+    url: '/product/global/shopee/draft.json',
+    params: {
+      bsStatus: 1,
+      shopId: '',
+      pageNo: 1,
+      pageSize: 50,
+      inquireType: 0,
+      searchType: '',
+      searchContent: '',
+      ...params,
+    },
+  })
 }
