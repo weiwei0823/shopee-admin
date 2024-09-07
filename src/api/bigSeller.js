bf6f39f28d0becffa30b57ec8449e88d4d39de0f
刘德伟 <liudewei0616@gmail.com>
2024-09-07 13:56:00 +0800

    提交信息

--- src/api/bigSeller.js
+++ src/api/bigSeller.js
@@ -1,18 +1,18 @@
 import httpBigSeller from '@/utils/bigsellerRequest'
 
-export function getBigSellerDraftBox( params ) {
-  return httpBigSeller.request( {
-    method : 'get',
-    url : '/getBigSellerDraftBox',
-    data : {
-      bsStatus : 1,
-      shopId : '',
-      pageNo : 1,
-      pageSize : 50,
-      inquireType : 0,
-      searchType : '',
-      searchContent : '',
-      ...params
-    }
-  } )
+export function getBigSellerDraftBox(params) {
+  return httpBigSeller.request({
+    method: 'get',
+    url: '/getBigSellerDraftBox',
+    data: {
+      bsStatus: 1,
+      shopId: '',
+      pageNo: 1,
+      pageSize: 50,
+      inquireType: 0,
+      searchType: '',
+      searchContent: '',
+      ...params,
+    },
+  })
 }
--- src/views/bigSeller/index.vue
+++ src/views/bigSeller/index.vue
@@ -1,32 +1,94 @@
 <template>
   <div class="bigSeller">
     <!--店铺表格-->
-    <el-table
-        ref="tableRef"
-        v-loading="draftListLoading"
-        :data="draftList"
-        border
-        fit
-        highlight-current-row
-        class="bigSellerTable"
+    <el-table ref="tableRef"
+              v-loading="draftListLoading"
+              :data="draftList"
+              border
+              fit
+              highlight-current-row
+              class="bigSellerTable"
     >
-      <!--编号-->
-      <el-table-column align="center" label="编号" min-width="50" fixed>
-        <template #default="{ row, $index }">
-          <el-tooltip class="shopTable-tooltip" placement="top">
+      <!--图片-->
+      <el-table-column align="center" label="图片" min-width="80">
+        <template #default="{ row }">
+          <el-image class="bigSeller-cell-image"
+                    :src="row.image"
+                    fit="cover"
+                    lazy
+                    :zoom-rate="1.2"
+                    :max-scale="7"
+                    :min-scale="0.2"
+                    :preview-src-list="[row.image]"
+                    :initial-index="0"
+                    :z-index="999"
+                    :preview-teleported="true"
+          />
+        </template>
+      </el-table-column>
+      <!--商品名称-->
+      <el-table-column align="center" label="商品名称" min-width="150" fixed>
+        <template #default="{ row }">
+          <el-tooltip class="bigSeller-tooltip" placement="top">
+            <span class="bigSeller-cell">
+              {{ row.name }}
+            </span>
             <template #content>
               <div>
-                <span class="shopTable-tooltip-title">商品ID:</span>
+                <span class="bigSeller-tooltip-title">编号:</span>
                 <span>{{ row.id }}</span>
               </div>
             </template>
           </el-tooltip>
         </template>
       </el-table-column>
-      <!--商品名称-->
-      <el-table-column align="center" label="商品名称" min-width="300">
+      <!--价格-->
+      <el-table-column align="center" label="价格" min-width="150" fixed>
         <template #default="{ row }">
-          <image :src="row.image"></image>
+          <el-tooltip class="bigSeller-tooltip" placement="top">
+            <div>
+              <div class="bigSeller-cell">
+                {{ row.originalPriceStr }}
+              </div>
+              <div class="bigSeller-cell">
+                {{ row.currency }}
+              </div>
+            </div>
+            <template #content>
+              <div>
+                <span class="bigSeller-tooltip-title">价格: </span>
+                <span>{{ row.originalPriceStr }}</span>
+              </div>
+              <div>
+                <span class="bigSeller-tooltip-title">货币: </span>
+                <span>{{ row.currency }}</span>
+              </div>
+            </template>
+          </el-tooltip>
+        </template>
+      </el-table-column>
+      <!--来源-->
+      <el-table-column align="center" label="商品来源" min-width="150" fixed>
+        <template #default="{ row }">
+          <el-tooltip class="bigSeller-tooltip" placement="top">
+            <div>
+              <div class="bigSeller-cell">
+                <a :href="row.sourceUrl" target="_blank">{{ row.sourcePlatform }}</a>
+              </div>
+              <div class="bigSeller-cell">
+                {{ row.sourceSite === "PH" ? "菲律宾" : row.sourceSite }}
+              </div>
+              <div class="bigSeller-cell">
+                <a :href="`https://www.bigseller.com/web/crawl/sourceSeeking/1688.htm?imageUrl=${encodeURIComponent(row.image)}`" target="_blank">{{ "1688寻找货源" }}</a>
+              </div>
+            </div>
+            <template #content>
+              <div>
+                <span class="bigSeller-tooltip-title">编号:</span>
+                <span>{{ row.id }}</span>
+              </div>
+            </template>
+          </el-tooltip>
         </template>
       </el-table-column>
     </el-table>
@@ -34,8 +96,8 @@
 </template>
 
 <script setup lang="ts">
-import { onBeforeMount, ref } from 'vue'
-import { getBigSellerDraftBox } from '@/api/bigSeller'
+import {onBeforeMount, ref} from 'vue'
+import {getBigSellerDraftBox} from '@/api/bigSeller'
 
 const draftList = ref( [] )
 const draftListLoading = ref( false )
@@ -56,5 +118,13 @@ onBeforeMount( () => {
 .bigSeller {
   width: 100%;
   height: 100%;
+
+  &-cell {
+
+    &-image {
+      width: 90px;
+      height: 90px;
+    }
+  }
 }
 </style>
